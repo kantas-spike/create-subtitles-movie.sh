78946f3d9be6c980be6b0c362e2968a2bceca547
kanta <101532773+kantas-spike@users.noreply.github.com>
2022-12-05 01:46:43 +0900

    refactor: 時間行の解析処理を関数化

--- my_srt.py
+++ my_srt.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+import json
+import os
+import re
 
 
 def time_to_delta(t):
@@ -9,25 +12,23 @@ def read_srt_file(path):
     item_list = []
     with open(path, encoding="utf-8") as f:
         item = {}
-        for line in f:
-            if len(line.rstrip()) == 0:
+        for line_with_sep in f:
+            line = line_with_sep.rstrip(os.linesep)
+            if len(line) == 0:
                 if len(item) > 0:
                     item_list.append(item)
                     item = {}
             elif len(item) == 0:
                 item['no'] = int(line)
             elif len(item) == 1:
-                time_info = line.split("-->")
-                if len(time_info) != 2:
+                if "-->" not in line:
                     raise ValueError(f"Bad time format:{item}")
-                item['time_info'] = []
-                for ti in time_info:
-                    item['time_info'].append(time_to_delta(datetime.strptime(ti.strip(), "%H:%M:%S,%f")))
+                item['time_info'] = parse_line_of_time(line)
             elif len(item) == 2:
                 item['lines'] = []
-                item['lines'].append(line.rstrip())
+                item['lines'].append(line)
             elif len(item) == 3:
-                item['lines'].append(line.rstrip())
+                item['lines'].append(line)
 
         if len(item) > 0:
             item_list.append(item)
@@ -36,4 +37,24 @@ def read_srt_file(path):
 
 
 def parse_line_of_time(line):
-    pass
+    m = re.match(r"\A(\d+:\d+:\d+,\d+) *--> *(\d+:\d+:\d+,\d+) *(.+)?", line)
+    results = {}
+    if m:
+        for k, t in [["start", m.group(1)], ["end", m.group(2)]]:
+            if t:
+                results[k] = time_to_delta(datetime.strptime(t, "%H:%M:%S,%f"))
+            else:
+                results[k] = None
+        if m.group(3):
+            extras = m.group(3)
+            if "JSON:" in extras:
+                json_data = json.loads(extras.split("JSON:")[1].strip())
+                # if "color" in json_data.keys():
+                #     if type(json_data["color"]) is str:
+                #         json_data["color"] = hex_to_rgba(json_data["color"])
+                results["json"] = json_data
+        return results
+    else:
+        return results
+
+
